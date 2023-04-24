05d86323f2578827833051efcb9640bd1d921b6b
Janderson Campelo <janderson.souza@becomex.com.br>
2023-04-24 20:47:26 -0300

    Criação dos DTOs de saída

---
+++ src/GuiaMercado.Application.Contracts/Categorias/CategoriaDto.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace GuiaMercado.Categorias
+{
+    public class CategoriaDto : AuditedEntityDto<long>
+    {
+        public string Descricao { get; set; }
+    }
+}
---
+++ src/GuiaMercado.Application.Contracts/Produtos/Dtos/ProdutoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace GuiaMercado.Produtos
+{
+    public class ProdutoDto : AuditedEntityDto<long>
+    {
+        public string Descricao { get; set; }
+        public decimal UltimoPreco { get; set; }
+        public DateTime UltimaCompra { get; set; }
+        public long CategoriaId { get; set; }
+        public string CategoriaDescricao { get; set; }
+    }
+}
