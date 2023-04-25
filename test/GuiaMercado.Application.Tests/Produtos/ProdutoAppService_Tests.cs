1b69480e4e6856dbf697d45ba3c534b6003c1a23
Janderson Campelo <janderson.souza@becomex.com.br>
2023-04-25 14:30:05 -0300

    Teste Unitário para ProdutoAppService

---
+++ test/GuiaMercado.Application.Tests/Produtos/ProdutoAppService_Tests.cs
@@ -0,0 +1,32 @@
+﻿using Shouldly;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Xunit;
+
+namespace GuiaMercado.Produtos
+{
+    public class ProdutoAppService_Tests : GuiaMercadoApplicationTestBase
+    {
+        private readonly IProdutoAppService _produtoAppService;
+
+        public ProdutoAppService_Tests()
+        {
+            _produtoAppService = GetRequiredService<IProdutoAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Get_Produto_List()
+        {
+            //Arrange
+
+            //Act
+            var output = await _produtoAppService.GetListAsync(
+                new PagedAndSortedResultRequestDto()
+            );
+
+            //Assert
+            output.TotalCount.ShouldBe(4);
+            output.Items.ShouldContain(x => x.Descricao == "Produto1");
+        }
+    }
+}
